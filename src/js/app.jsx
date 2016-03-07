260cd0a6cc62f11b3feb4551f69fb544c997b427
Heikki Partanen <heikki.partanen@gmail.com>
2016-03-07 21:31:35 +0200

    Working drag and drop game form

--- src/js/app.jsx
+++ src/js/app.jsx
@@ -3,13 +3,27 @@
 var _ = require('lodash');
 var React = require('react');
 var ReactDOM = require('react-dom');
+var Modal = require('react-modal');
 
+var dragula = require('react-dragula');
 var Plotly = require('react-plotlyjs');
 
 import createBrowserHistory from 'history/lib/createBrowserHistory'
 import { Router, Route, Link, IndexRoute } from 'react-router'
 
 import { Login, Logout, auth } from './login'
+
+var modalStyle = {
+  content: {
+    top                   : '25%',
+    //width                 : '80%',
+    left                  : '25%',
+    right                 : '25%',
+    bottom                : 'auto',
+    //marginRight           : '-25%',
+    //transform             : 'translate(-25%, -25%)'
+  }
+};
 
 var PlayerStatsRow = React.createClass({
   render: function() {
@@ -70,7 +84,7 @@ var Player = React.createClass({
   },
   render: function() {
     return (
-      <div className="playerDiv">
+      <div className="row playerDiv">
         <h1>{this.state.name}</h1>
         <PlayerStatsTable players={[this.state.player]} />
       </div>
@@ -217,9 +231,105 @@ var GameList = React.createClass({
   }
 });
 
-var GameForm = React.createClass({
-  getInitialState: function() {
-    return {state: ""};
+var GameDragSourceContainer = React.createClass({
+  render: function () {
+    return (
+      <div className={"col-sm-" + (this.props.width || 4) + " " + (this.props.extraClass || "")}>
+        {this.props.players}
+      </div>
+    );
+  },
+});
+var GameDragTargetContainer = React.createClass({
+  render: function () {
+    return (
+      <div className={"col-sm-" + (this.props.width || 4) + " " + (this.props.extraClass || "")}>
+        {this.props.players}
+      </div>
+    );
+  },
+});
+
+var GameDragForm = React.createClass({
+  getInitialFormState: function () {
+    return {
+      modalIsOpen: false,
+      state: "",
+      playersHome: [],
+      playersAway: [],
+      playersIdle: _.map(this.props.players, 'name'),
+    };
+  },
+  getInitialState: function () {
+    return _.merge({
+      drake: dragula(),
+      dragContainers: {}
+    }, this.getInitialFormState());
+  },
+  addComponentToDrake: function (item) {
+    if (!item) {
+      return;
+    };
+    var domNode = ReactDOM.findDOMNode(item);
+    this.state.dragContainers[item.props.playerSink] = domNode;
+    this.state.drake.containers.push(domNode);
+  },
+  onDraggableClick: function (name) {
+    var self = this;
+    var dragItem = self.refs["dragItem" + name];
+    var sourceContainer = document.querySelector(".dragSource");
+    var sourceSink, targetSink;
+    var newState = _.pick(self.state, ["playersIdle", "playersHome", "playersAway"]);
+
+    if (dragItem.parentElement === sourceContainer) {
+      return;
+    }
+
+    // Update state by moving the player back to dragSource
+    _.forOwn(self.state.dragContainers, function (o, playerSink) {
+      if (o === dragItem.parentElement) {
+        sourceSink = playerSink;
+      }
+      if (o === sourceContainer) {
+        targetSink = playerSink;
+      }
+    });
+    if (targetSink !== sourceSink) {
+      _.pull(newState[sourceSink], name);
+      newState[targetSink].push(name);
+      self.setState(newState);
+    }
+  },
+  openModal: function () {
+    this.setState({modalIsOpen: true});
+  },
+  closeModal: function () {
+    this.setState(this.getInitialFormState());
+  },
+  componentDidMount: function () {
+    var self = this;
+
+    // Update state on drop event
+    this.state.drake.on('drop', function (el, target, source, sibling) {
+      var sourceSink, targetSink;
+      var newState = _.pick(self.state, ["playersIdle", "playersHome", "playersAway"]);
+      self.state.drake.cancel(true);
+
+      _.forOwn(self.state.dragContainers, function (o, playerSink) {
+        if (o === source) {
+          sourceSink = playerSink;
+        }
+        if (o === target) {
+          targetSink = playerSink;
+        }
+      });
+      if (targetSink !== sourceSink) {
+        _.pull(newState[sourceSink], el.textContent);
+        newState[targetSink].push(el.textContent);
+        self.setState(newState);
+      }
+    });
+
   },
   sendFormData: function () {
     var data = {
@@ -227,17 +337,22 @@ var GameForm = React.createClass({
       teamAway: this.state.teamAway,
       goalsHome: this.state.goalsHome,
       goalsAway: this.state.goalsAway,
-      playersHome: _.map(this.state.playersHome.split(','), _.trim).join(','),
-      playersAway: _.map(this.state.playersAway.split(','), _.trim).join(','),
+      playersHome: _.map(this.state.playersHome, _.trim).join(','),
+      playersAway: _.map(this.state.playersAway, _.trim).join(','),
       series: this.props.series,
     };
+    if (_.some(data, _.isEmpty)) {
+      this.setState({error: "Check the game data"});
+      return;
+    }
     $.ajax({
       type: "POST",
       url: PSG_API_URL + "/game",
       data: data,
       dataType: 'json',
       success: function (data) {
-        this.replaceState(this.getInitialState());
+        this.closeModal();
+        this.props.onGameChange();
       }.bind(this),
       error: function (xhr, status, err) {
         console.error(xhr, status, err.toString());
@@ -253,25 +368,51 @@ var GameForm = React.createClass({
     newState[event.target.name] = event.target.value;
     this.setState(newState);
   },
-  render: function() {
+  render: function () {
+    var self = this;
+    var playerList = function (players) {
+      return _.map(players, function (p) {
+        return (
+          <span onClick={self.onDraggableClick.bind(self, p)} key={p} ref={"dragItem" + p} className={"dragItem btn btn-info dragItem" + p}>{p}</span>
+        );
+      });
+    }
+
     return (
-      <form className="form-inline" action="" onSubmit={this.handleSubmit} >
-        <table className="table">
-          <tbody>
-            <tr>
-              <td><input name="playersAway" type="text" onChange={this.handleChange} value={this.state.playersAway} /></td>
-              <td><input name="teamAway" type="text" size="4" onChange={this.handleChange} value={this.state.teamAway} /></td>
-              <td><input name="goalsAway" type="text" size="2" onChange={this.handleChange} value={this.state.goalsAway} /></td>
-              <td><input name="goalsHome" type="text" size="2" onChange={this.handleChange} value={this.state.goalsHome} /></td>
-              <td><input name="teamHome" type="text" size="4" onChange={this.handleChange} value={this.state.teamHome} /></td>
-              <td><input name="playersHome" type="text" onChange={this.handleChange} value={this.state.playersHome} /></td>
-            </tr>
-            <tr><td><button type="submit" className="btn btn-primary">Save</button></td></tr>
-          </tbody>
-        </table>
-      </form>
+      <div className="container">
+        <button className="btn btn-info" onClick={this.openModal}>Add new game</button>
+        <Modal isOpen={this.state.modalIsOpen} onRequestClose={this.closeModal} style={modalStyle}>
+          <h4>Drag players to teams. Clicking on a player removes him from the team.</h4>
+          <div className="row">
+            <GameDragSourceContainer playerSink="playersIdle" ref={this.addComponentToDrake} width="12" extraClass="text-center dragSource" players={playerList(this.state.playersIdle)} />
+          </div>
+          <form role="form-horizontal" action="" onSubmit={this.handleSubmit}>
+            <div className="row dragRow">
+              <GameDragTargetContainer playerSink="playersAway" ref={this.addComponentToDrake} extraClass="dragContainer" players={playerList(this.state.playersAway)} />
+              <div className="col-sm-2 form-group">
+                <label htmlFor="teamAway">Away team</label>
+                <input name="teamAway" className="form-control" type="text" size="4" onChange={this.handleChange} value={this.state.teamAway} />
+                <label htmlFor="goalsAway">Goals</label>
+                <input name="goalsAway" className="form-control" type="text" size="2" onChange={this.handleChange} value={this.state.goalsAway} />
+              </div>
+              <div className="col-sm-2 text-right form-group">
+                <label htmlFor="teamHome">Home team</label>
+                <input name="teamHome" className="form-control" type="text" size="4" onChange={this.handleChange} value={this.state.teamHome} />
+                <label htmlFor="goalsHome">Goals</label>
+                <input name="goalsHome" className="form-control" type="text" size="2" onChange={this.handleChange} value={this.state.goalsHome} />
+              </div>
+              <GameDragTargetContainer playerSink="playersHome" ref={this.addComponentToDrake} extraClass="dragContainer" players={playerList(this.state.playersHome)} />
+            </div>
+            <span className="text-warning">{this.state.error}</span>
+            <div className="form-group text-center">
+              <button onClick={this.closeModal} className="btn btn-link">Cancel</button>
+              <button type="submit" className="btn btn-primary">Save</button>
+            </div>
+          </form>
+        </Modal>
+      </div>
     );
-  }
+  },
 });
 
 var SeriesStatsChooser = React.createClass({
@@ -335,7 +476,7 @@ var Series = React.createClass({
       this.getSeriesStats();
     }
   },
-  componentDidMount: function () {
+  refreshStats: function () {
     this.getSeriesStats();
     $.ajax({
       url: PSG_API_URL + "/series/" + this.props.params.name + "/games",
@@ -349,6 +490,9 @@ var Series = React.createClass({
       }.bind(this),
     });
   },
+  componentDidMount: function () {
+    this.refreshStats();
+  },
   render: function() {
     if (_.isEmpty(this.state.players)) {
       return (
@@ -357,13 +501,13 @@ var Series = React.createClass({
       )
     }
     return (
-      <div className="seriesDiv">
+      <div className="container">
         <h2>{this.state.name}</h2>
         <SeriesStatsChooser tabs={this.state.tabs} activeTabId={this.state.activeTabId} onTabClick={this.onTabClick} />
         <PlayerList players={this.state.players} />
         <PlayerStreakGraph players={this.state.players} />
         <GameList games={this.state.games} />
-        { auth.loggedIn() ? <GameForm series={this.state.name} /> : null }
+        { auth.loggedIn() ? <GameDragForm onGameChange={this.refreshStats} series={this.state.name} players={this.state.players} /> : null }
       </div>
     );
   }
