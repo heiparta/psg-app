b87d76bb48c01fec7cc35e94456a5a9a1bdcead0
Heikki Partanen <heikki.partanen@f-secure.com>
2017-03-26 15:06:26 +0300

    Disable streak graph until streaks are working

--- src/js/app.jsx
+++ src/js/app.jsx
@@ -520,7 +520,6 @@ var Series = React.createClass({
         <h2>{this.state.name}</h2>
         <SeriesStatsChooser tabs={this.state.tabs} activeTabId={this.state.activeTabId} onTabClick={this.onTabClick} />
         <PlayerList players={this.state.players} />
-        <PlayerStreakGraph players={this.state.players} />
         <GameList games={this.state.games} />
         { auth.loggedIn() ? <GameDragForm onGameChange={this.refreshStats} series={this.state.key} players={this.state.players} /> : null }
       </div>
